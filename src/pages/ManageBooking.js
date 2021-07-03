d7956a541aa047928d014e682a00198952c99361
Gift <gkenny896@gmail.com>
2021-07-03 01:07:28 +0100

    Booking UI almost done, this is a test for mobile

--- src/pages/ManageBooking.js
+++ src/pages/ManageBooking.js
@@ -1,62 +1,101 @@
 import { Link } from 'react-router-dom';
+import Logo from '../images/ng-abctra-logo.png';
+import Lagos from '../images/State destinations/lagos.jpg';
 
 const ManageBooking = () => {
     return (
-        <div className='' style={{ fontSize: 'small' }}>
-            <div
-                className='d-sm-flex text-center justify-content-sm-between align-items-center w-100 px-sm-5 px-1 py-2'
-                style={{ background: '#00913e' }}
+        <div
+            className=''
+            style={{
+                fontSize: 'small',
+                backgroundImage: `url(${Lagos})`,
+                backgroundPosition: '100% 40%',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                minHeight: '100vh',
+            }}
+        >
+            <nav
+                className='navbar navbar-expand-md justify-content-between px-5 navbar-light'
+                style={{ backgroundColor: 'rgba(5, 190, 83, 0.3)' }}
             >
-                <div className=''>
-                    <span className='pr-2 d-flex justify-content-center d-sm-inline-flex'>
-                        <i className='bi bi-envelope-fill text-white'></i>{' '}
-                        <a
-                            className='text-white'
-                            href='mailto:gkenny896@gmail.com'
-                            style={{ textDecoration: 'none' }}
-                        >
-                            gkenny896@gmail.com
-                        </a>
-                    </span>
-                    <span className='pl-4 d-flex justify-content-center d-sm-inline-flex'>
-                        <i className='bi bi-telephone-fill text-white'></i>{' '}
-                        <a
-                            className='text-white'
-                            href='tel:07045583524'
-                            style={{ textDecoration: 'none' }}
-                        >
-                            +234 704 558 3524
-                        </a>
-                    </span>
+                <Link to='/' className='navbar-brand' href='#'>
+                    <img src={Logo} alt='Abc Transport' width={50} />
+                </Link>
+                <button
+                    className='navbar-toggler'
+                    type='button'
+                    data-toggle='collapse'
+                    data-target='#navbarSupportedContent'
+                    aria-controls='navbarSupportedContent'
+                    aria-expanded='false'
+                    aria-label='Toggle navigation'
+                >
+                    <span className='navbar-toggler-icon'></span>
+                </button>
+
+                <div
+                    className='collapse navbar-collapse'
+                    id='navbarSupportedContent'
+                >
+                    <ul className='navbar-nav mr-auto'>
+                        <li className='nav-item active'>
+                            <span className='d-flex align-items-center'>
+                                <i className='bi bi-envelope-fill'></i>
+                                <a
+                                    href='mailto:gkenny896@gmail.com'
+                                    className='nav-link'
+                                >
+                                    gkenny896@gmail.com
+                                </a>
+                            </span>
+                        </li>
+                        <li className='nav-item'>
+                            <span className='d-flex align-items-center'>
+                                <i className='bi bi-telephone-fill'></i>{' '}
+                                <a className='nav-link' href='tel:07045583524'>
+                                    +234 704 558 3524
+                                </a>
+                            </span>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/agent/login' className='nav-link'>
+                                Agent login
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/agent/registration' className='nav-link'>
+                                Agent registration
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
-                <div>
-                    <Link
-                        to='/agent/login'
-                        className='text-white pr-2 d-flex justify-content-center d-sm-inline-flex'
+            </nav>
+
+            <div
+                className='row mx-1 mx-sm-5 my-5'
+                style={{ fontSize: 'larger' }}
+            >
+                <div className='col-md-10 col-lg-8'>
+                    <div
+                        className='card p-md-5 py-5 px-3'
+                        style={{ background: 'rgba(10, 9, 62, 0.5)' }}
                     >
-                        Agent Login
-                    </Link>
-                    <Link
-                        to='/agent/registration'
-                        className='text-white pl-4 d-flex justify-content-center d-sm-inline-flex'
-                    >
-                        Agent Registration
-                    </Link>
+                        <div className>
+                            <label className='text-white'>
+                                Reference Number
+                            </label>
+                            <input type='text' className='form-control mb-4' />
+                        </div>
+                        <div>
+                            <label className='text-white'>
+                                Email/Phone Number
+                            </label>
+                            <input type='text' className='form-control' />
+                        </div>
+                    </div>
                 </div>
             </div>
-            {/* <div className='card'>
-                <h5 className='card-header'>Featured</h5>
-                <div className='card-body'>
-                    <h5 className='card-title'>Special title treatment</h5>
-                    <p className='card-text'>
-                        With supporting text below as a natural lead-in to
-                        additional content.
-                    </p>
-                    <Link to='#' className='btn btn-primary'>
-                        Go somewhere
-                    </Link>
-                </div>
-            </div> */}
         </div>
     );
 };
