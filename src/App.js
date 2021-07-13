47c64dfe8622b7ee0899a5e23de3ca3b6f5ccac6
Gift <gkenny896@gmail.com>
2021-07-13 16:58:07 +0100

    2. about page; unused variables removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useContext } from 'react';
+// import { useEffect, useContext } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
